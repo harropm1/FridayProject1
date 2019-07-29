7443f3a8586d4404728320cfebb2886dbb02d1c3
Michelle Harrop <harrop.michelle@gmail.com>
2019-07-29 12:04:54 -0400

    made a whole bunch of changes

--- scripts/futureValueCalculator.js
+++ scripts/futureValueCalculator.js
@@ -1,6 +1,6 @@
-//These are the scripts for the future value calculations
 "use strict";
 
+/* This is how I initially did the calculations, but I was able to work out how to change it.
 function getFutureValue(depositAmount, interestRate, loanLength)
 {
     let futureValue = depositAmount * (Math.pow((1 + (interestRate/1200)), (loanLength * 12)));
@@ -43,5 +43,67 @@ function init()
     btnCalculate.onclick = calculate;
 }
 
+window.onload = init; */
 
-window.onload = init;
+/* These are the scripts for the calculations related to a future value of a one-time deposit assuming compounded interest.
+*
+* @param depositAmount (Number) - the amount of the inital deposit
+* @param interestRate (Number) - the interest rate
+* @param loanLength (Number) - the length of time the loan is in the account in years
+*
+* @return futureValue (Number) - the amount of the future value of the deposit
+* @return interestEarned (Number) - the amount of interest earned on the initial deposit
+* 
+* Below is the calculation for the future value of the deposit
+*/
+function getFutureValue(depositAmount, interestRate, loanLength)
+{
+    let futureValue = depositAmount * (Math.pow((1 + (interestRate/1200)), (loanLength * 12)));
+    return futureValue;
+}
+
+/* Below is the calculation for the interest earned on the deposit */
+function getInterestEarned(depositAmount, futureValue)
+{
+    let interestEarned = futureValue - depositAmount;
+    return interestEarned;
+}
+
+window.onload = function()
+{
+    let depositAmountField;
+    let interestRateField;
+    let loanLengthField;
+    let futureValueField;
+    let interestEarnedField;
+
+    depositAmountField = document.getElementById("depositAmount");
+    interestRateField = document.getElementById("interestRate");
+    loanLengthField = document.getElementById("loanLength");
+    futureValueField = document.getElementById("futureValue");
+    interestEarnedField = document.getElementById("interestEarned");
+
+    const btnCalculate = document.getElementById("calculate");
+    btnCalculate.onclick = function()
+    {
+        let futureValue = getFutureValue(Number(depositAmountField.value), Number(interestRateField.value), Number(loanLengthField.value));
+            if (isNaN(futureValue) == true || (futureValue) <= 0)
+            {
+                alert("One or more of your fields is not a positive number or has a non-numberic character.");
+            }
+            else
+            {
+                futureValueField.value = futureValue.toFixed(2);
+            }
+
+        let interestEarned = getInterestEarned(Number(depositAmountField.value), futureValue);
+            if (isNaN(interestEarned) == true || (interestEarned) <= 0)
+            {
+                alert("One or more of your fields is not a positive number or has a non-numberic character.");
+            }
+            else
+            {
+                interestEarnedField.value = interestEarned.toFixed(2);
+            }
+    }
+}
